e751716b4fa5a9ef64fec4ed81ff6e68f364a5c5
서강민 <tjrkdals613@gmail.com>
2017-03-14 13:00:25 +0900

    실습과제2 제출

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_Operator
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 14.
+ * </pre>
+ * 
+ * @author : 강민
+ * @version : 1.0
+ */
+public class Operator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int money = 1700000;
+		int people = 3;
+		int store = 1500;
+
+		long result = (long)money*people*store*12;
+		
+		System.out.println("<<디미베네 연간 인건비>>");
+		System.out.println("월 평균 급여 : "+String.format("%,d", money)+"원");
+		System.out.println("점포 내 직원 수 : "+String.format("%,d", people)+"명");
+		System.out.println("점포 수 : "+String.format("%,d", store)+"개");
+		System.out.println("");
+		System.out.println("연간 인건비 : "+String.format("%,d", result)+"원");
+	}
+
+}
