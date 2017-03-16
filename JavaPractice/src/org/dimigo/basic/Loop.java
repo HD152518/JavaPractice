5c4c8903752c3d9af008d02b816ac1e7706991ba
서강민 <tjrkdals613@gmail.com>
2017-03-16 09:17:28 +0900

    실습과제3 제출

---
+++ JavaPractice/src/org/dimigo/basic/Loop.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_Loop
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 16.
+ * </pre>
+ * 
+ * @author : 강민
+ * @version : 1.0
+ */
+public class Loop {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random rand = new Random();
+		int choice;
+		int power = 100;
+		String[] job = {"마법사", "영주", "기사", "농민"};
+		
+		while(true){
+			System.out.println("-------------------------");
+			System.out.println("<< 게임 메뉴 >>");
+			System.out.println("1. 공격력 증가");
+			System.out.println("2. 공격력 감소");
+			System.out.println("3. 캐릭터 설정");
+			System.out.println("9. 종료");
+			System.out.println("-------------------------");
+			System.out.print("메뉴 입력 =>");
+			choice = sc.nextInt();
+			
+			switch(choice){
+			case 1:
+				power+=10;
+				System.out.println("공격력이 증가되었습니다. 현재 공격력 : "+power);
+				break;
+			case 2:
+				power-=10;
+				System.out.println("공격력이 감소되었습니다. 현재 공격력 : "+power);
+				break;
+			case 3:
+				System.out.println(job[rand.nextInt(4)] + "(으)로 설정되어습니다.");
+				break;
+			case 9:
+				System.out.println("이제 공부하세요!");
+				return;
+			default:
+				System.out.println("없는 메뉴입니다!!");
+				break;
+			}
+		}
+
+	}
+
+}
