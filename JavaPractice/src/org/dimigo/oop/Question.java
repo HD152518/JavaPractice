c1ef7a760a2376b90ce09e1abb4403d211f59f1a
서강민 <tjrkdals613@naver.com>
2017-03-31 14:34:44 +0900

    실습과제5 제출

---
+++ JavaPractice/src/org/dimigo/oop/Question.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+import java.util.Scanner;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_Question
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 31.
+ * </pre>
+ * 
+ * @author : 강민
+ * @version : 1.0
+ */
+public class Question {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String[] questions = { "가장 좋아하는 가수는? ", "가장 좋아하는 배우는? ", "가장 좋아하는 과목은? " };
+		String[] answers = { "빅뱅", "공유", "응용 프로그래밍" };
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(""+(i+1)+"."+questions[i]);
+			String myAnswer = sc.nextLine();
+			
+			if(myAnswer.equals(answers[i]))
+				System.out.println("정답입니다!");
+			else
+				System.out.println("틀렸습니다!");
+		}
+		
+		System.out.println("<< 결과 출력 >>");
+		
+		for(int i=0;i<3;i++)
+			System.out.println(
+					new StringBuilder(questions[i]).append(answers[i]).append("입니다."));
+	}
+}
