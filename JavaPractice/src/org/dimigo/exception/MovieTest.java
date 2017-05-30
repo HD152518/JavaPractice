9a100cf9e01580a8506385185bf2a6a8dc4ab4f6
HD152518 <tjrkdals613@naver.com>
2017-05-30 12:39:51 +0900

    Add files via upload

--- JavaPractice/src/org/dimigo/exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/exception/MovieTest.java
@@ -22,8 +22,13 @@ public class MovieTest {
 				new Movie("가디언즈 오브 갤럭시",12)
 		};
 		int age=13;
+		
 		for(Movie movie : movies) {
-			movie.buyTicket(age);
+			try{
+				movie.buyTicket(age);
+			} catch(Exception e) {
+				System.out.println(e.getMessage());
+			}
 		}
 	}
 }
